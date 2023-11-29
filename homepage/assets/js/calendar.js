eb2c29d34086857faed4cec8df83b03837091da2
YuunJiee <h245364612@gmail.com>
2023-11-29 23:51:58 +0800

    update

--- homepage/assets/js/calendar.js
+++ homepage/assets/js/calendar.js
@@ -88,7 +88,6 @@ function closeModal() {
   backDrop.style.display = 'none';
   eventTitleInput.value = '';
   clicked = null;
-  load();
 }
 
 function saveEvent() {
@@ -116,10 +115,13 @@ function deleteEvent() {
 function openNewEventBox(){
   const pickDayDiv = document.getElementById("currentDay");
   const pickDay = pickDayDiv.getAttribute('data-day');
-  console.log(pickDay);
   const modalBackDrop = document.getElementById('modalBackDrop');
   newEventModal.style.display = 'grid';
-
+  const startTimeInput = document.getElementById("startTime");
+  const endTimeInput = document.getElementById("endTime");
+  startTimeInput.value = pickDay + "T00:00";
+  endTimeInput.value = pickDay + "T23:59";
+  
   modalBackDrop.addEventListener('click', (event) => {
     if (!event.target.classList.contains('event')) {
       closeModal(); // 點擊非事件區域時關閉視窗
@@ -208,14 +210,16 @@ function showAMonthCalendar(allEventJson, m){
 function showADayEvent(allEventJson, d){
   $('#EventBox').empty();
   allEventJson.forEach(function(event){
-      if(event.startTime.includes(d)){
-          $('#EventBox').append(createEventDiv(event));
-      }
+        $('#EventBox').append(createEventDiv(event, d));
   })
 }
 
+// TODO: 修改，多天事件發生時
 function addPointToCalendar(event){
   const startTimeDate = new Date(event.startTime);
+  const endTimeDate = new Date(event.endTime);
+
+
   const startYear = startTimeDate.getFullYear();
   const startMonth = startTimeDate.getMonth();
   const startDay = startTimeDate.getDate();
@@ -227,7 +231,7 @@ function addPointToCalendar(event){
   eventPointDiv.appendChild(pointDiv);
 }
 
-function createEventDiv(event){
+function createEventDiv(event, day){
   var $eventDiv= $('<div>').addClass('event');
   var $eventTimeDiv = $('<div>').addClass('eventTime');
 
@@ -235,19 +239,61 @@ function createEventDiv(event){
   const startH = startTimeDate.getHours();
   const startM = startTimeDate.getMinutes();
   const formattedStartTime = `${startH.toString().padStart(2, '0')}:${startM.toString().padStart(2, '0')}`;
-  var $eventStartDiv = $('<div>').addClass('eventStart').text(formattedStartTime);
+  var $eventStartDiv = $('<div>').addClass('eventStart');
 
   const endTimeDate = new Date(event.endTime);
   const endH = endTimeDate.getHours();
   const endM = endTimeDate.getMinutes();
   const formattedEndTime = `${endH.toString().padStart(2, '0')}:${endM.toString().padStart(2, '0')}`;
-  var $eventEndDiv = $('<div>').addClass('eventEnd').text(formattedEndTime);
+  var $eventEndDiv = $('<div>').addClass('eventEnd');
+
+  const theDay = new Date(day);
+
+  // 判斷是否為多天的行程
+  if(isSameDay(startTimeDate, endTimeDate)){
+
+    // 判斷是否為整天行程
+    if(startH === 0 && startM === 0 && endH === 23 && endM === 59){
+      $eventStartDiv.text('All');
+      $eventEndDiv.text('day');
+    }else{
+      $eventStartDiv.text(formattedStartTime);
+      $eventEndDiv.text(formattedEndTime);
+    }
+
+  }else{
+
+    const sD = startTimeDate.getDate();
+    const eD = endTimeDate.getDate();
+    const DD = theDay.getDate();
+
+    // 為多天的第一天
+    if(sD === DD){
+      $eventStartDiv.text(formattedStartTime);
+      $eventEndDiv.text('23:59');
+    // 為多天的最後一天
+    }else if(eD === DD){
+      $eventStartDiv.text('00:00');
+      $eventEndDiv.text(formattedEndTime);
+    // 為區間內
+    }else{
+      $eventStartDiv.text('All');
+      $eventEndDiv.text('day'); 
+    }
+    
+  }
 
   $eventTimeDiv.append($eventStartDiv, $eventEndDiv);
   var $eventTagColorDiv = $('<div>').addClass('eventTagColor');
   var $eventNameP = $('<p>').addClass('eventName').text(event.name);
   $eventDiv.append($eventTimeDiv, $eventTagColorDiv, $eventNameP);
   return $eventDiv
+}
+
+function isSameDay(date1, date2) {
+  return date1.getFullYear() === date2.getFullYear() &&
+         date1.getMonth() === date2.getMonth() &&
+         date1.getDate() === date2.getDate();
 }
 
 function showTheSelectBox() {
