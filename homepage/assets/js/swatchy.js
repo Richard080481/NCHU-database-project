e0416d54d009e959f61528684f344d8c94b5cc00
Richard080481 <richard20031126@gmail.com>
2023-12-04 22:11:19 +0800

    change the colorpicker version2

--- homepage/assets/js/swatchy.js
+++ homepage/assets/js/swatchy.js
@@ -1,78 +1,86 @@
 let hasSwatchyRun = false;
 
-/**
- *
- * @param {Boolean} [autoClose=false] - Should the color picker close after selecting a color?
- * @param {Array} [watches] - Custom color swatches to use. Enter an array of hex codes
- * @constructor
- */
-
 function Swatchy(
-    autoClose=true,
-    swatches=[
-    "#64CCC5","#6a1b9a","#f584d3","#f20707","#d11d9b", //青 紫 粉 紅 桃
-    "#f5e50a","#1af50a","#f2a90c","#ed8251","#5166ed", //黃 綠 橘 橙 藍
-    "#6d6a8f","#40f7b1","#000000","#756937","#11038c", //灰 白 黑 褐 深藍
+    autoClose = true,
+    swatches = [
+        { color: "#64CCC5", tag: "Cya" },   // 青色
+        { color: "#6a1b9a", tag: "Pur" },   // 紫色
+        { color: "#f584d3", tag: "Pin" },   // 粉红色
+        { color: "#f20707", tag: "Red" },   // 红色
+        { color: "#d11d9b", tag: "Mag" },   // 洋红色
+        { color: "#f5e50a", tag: "Yel" },   // 黃色
+        { color: "#1af50a", tag: "Gre" },   // 綠色
+        { color: "#f2a90c", tag: "Ora" },   // 橙色
+        { color: "#ed8251", tag: "Pch" },   // 桃色
+        { color: "#5166ed", tag: "Blu" },  // 藍色
+        { color: "#6d6a8f", tag: "Gra" },   // 灰色
+        { color: "#40f7b1", tag: "Whi" },   // 白色
+        { color: "#000000", tag: "Bla" },   // 黑色
+        { color: "#756937", tag: "Bro" },   // 褐色
+        { color: "#11038c", tag: "DBl" },   // 深藍色
     ]
 ) {
-
     if (!hasSwatchyRun) {
         hasSwatchyRun = true;
         let swatchyCount = document.querySelectorAll('.swatchy-trigger');
 
         for (let id = 0; id < swatchyCount.length; id++) {
+            let output;
+            let container;
 
-            let output
-            let container
-
-            // swatches = props?.swatches ?? this.swatches
-
-            document.querySelectorAll('.swatchy-trigger').item(id)
-            document.querySelectorAll('.swatchy-trigger').item(id).addEventListener('click', togglePopup)
-            output = document.querySelectorAll('.swatchy-output').item(id)
+            document.querySelectorAll('.swatchy-trigger').item(id).addEventListener('click', togglePopup);
+            output = document.querySelectorAll('.swatchy-output').item(id);
 
             // create popup element
-            container = document.createElement("div")
-            container.classList.add('swatchy-element')
-            container.setAttribute('style', 'display: none;')
-            output.after(container)
+            container = document.createElement("div");
+            container.classList.add('swatchy-element');
+            container.setAttribute('style', 'display: none;');
+            output.after(container);
 
             // add swatches to popup
-            let swatchContainer = document.createElement('div')
-            swatchContainer.classList.add('swatchy-swatches')
-            container.appendChild(swatchContainer)
+            let swatchContainer = document.createElement('div');
+            swatchContainer.classList.add('swatchy-swatches');
+            container.appendChild(swatchContainer);
 
-            let swatchCount = -1;
             for (const swatch of swatches) {
-                swatchCount++;
-                let colorButton = document.createElement('div')
-                colorButton.setAttribute('data-swatchy-color', swatch)
-                colorButton.style.backgroundColor = swatch
-                colorButton.classList.add('swatchy-color-button')
-                colorButton.addEventListener('click', selectColor)
-                swatchContainer.appendChild(colorButton)
+                let colorButton = document.createElement('div');
+                colorButton.setAttribute('data-swatchy-color', swatch.color);
+                colorButton.style.backgroundColor = swatch.color;
+                colorButton.classList.add('swatchy-color-button');
+                colorButton.addEventListener('click', selectColor);
 
+                // Create a tag element for each swatch
+                let tag = document.createElement('div');
+                tag.classList.add('swatchy-tag');
+                tag.innerText = swatch.tag;
+
+                // Append the tag and color button to the swatch container
+                swatchContainer.appendChild(tag);
+                swatchContainer.appendChild(colorButton);
             }
 
+            let editBut = document.createElement('button');
+            editBut
+
             function selectColor(e) {
-                let input = document.querySelectorAll('.swatchy-output').item(id)
-                let newColor = e.target.getAttribute('data-swatchy-color')
-                input.setAttribute('value', newColor)
-                input.setAttribute('data-swatchy-color', newColor)
-                input.setAttribute('style', 'background-color: ' + newColor + '; color: ' + newColor + ';')
+                let input = document.querySelectorAll('.swatchy-output').item(id);
+                let newColor = e.target.getAttribute('data-swatchy-color');
+                input.setAttribute('value', newColor);
+                input.setAttribute('data-swatchy-color', newColor);
+                input.setAttribute('style', 'background-color: ' + newColor + '; color: ' + newColor + ';');
                 if (autoClose) {
-                    togglePopup()
+                    togglePopup();
                 }
             }
 
             function togglePopup() {
-                let el = document.querySelectorAll('.swatchy-element').item(id)
+                let el = document.querySelectorAll('.swatchy-element').item(id);
 
-                let display = (window.getComputedStyle ? getComputedStyle(el, null) : el.currentStyle).display
+                let display = (window.getComputedStyle ? getComputedStyle(el, null) : el.currentStyle).display;
                 if ('none' === display) {
-                    el.style.display = 'block'
+                    el.style.display = 'block';
                 } else {
-                    el.style.display = 'none'
+                    el.style.display = 'none';
                 }
             }
         }
@@ -80,5 +88,5 @@ function Swatchy(
         console.info('You only need to call swatchy once per page');
     }
 }
-Swatchy();
 
+Swatchy();
